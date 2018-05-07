0029abe698ff6b83b33cc2b74a35d7586855449e
Pep Mendez <jmendez1@xtec.cat>
2018-05-07 14:36:55 +0200

    settings in progress: general settings

--- app/src/main/java/cat/cristina/pep/jbcnconffeedback/activity/SettingsActivity.kt
+++ app/src/main/java/cat/cristina/pep/jbcnconffeedback/activity/SettingsActivity.kt
@@ -8,16 +8,12 @@ import android.media.RingtoneManager
 import android.net.Uri
 import android.os.Build
 import android.os.Bundle
-import android.preference.ListPreference
-import android.preference.Preference
-import android.preference.PreferenceActivity
-import android.preference.PreferenceFragment
-import android.preference.PreferenceManager
-import android.preference.RingtonePreference
+import android.preference.*
+import android.support.v4.app.NavUtils
 import android.text.TextUtils
 import android.view.MenuItem
-import android.support.v4.app.NavUtils
 import cat.cristina.pep.jbcnconffeedback.R
+import cat.cristina.pep.jbcnconffeedback.utils.PreferenceKeys
 
 /**
  * A [PreferenceActivity] that presents a set of application settings. On
@@ -44,12 +40,13 @@ class SettingsActivity : AppCompatPreferenceActivity() {
     }
 
     override fun onMenuItemSelected(featureId: Int, item: MenuItem): Boolean {
-        val id = item.itemId
-        if (id == android.R.id.home) {
-            if (!super.onMenuItemSelected(featureId, item)) {
-                NavUtils.navigateUpFromSameTask(this)
+        when (item.itemId) {
+            android.R.id.home -> {
+                if (!super.onMenuItemSelected(featureId, item)) {
+                    NavUtils.navigateUpFromSameTask(this)
+                }
+                return true
             }
-            return true
         }
         return super.onMenuItemSelected(featureId, item)
     }
@@ -95,6 +92,7 @@ class SettingsActivity : AppCompatPreferenceActivity() {
             // to their values. When their values change, their summaries are
             // updated to reflect the new value, per the Android Design
             // guidelines.
+            bindPreferenceSummaryToValue(findPreference(PreferenceKeys.ANIMATION_KEY))
             bindPreferenceSummaryToValue(findPreference("example_text"))
             bindPreferenceSummaryToValue(findPreference("example_list"))
         }
@@ -107,7 +105,7 @@ class SettingsActivity : AppCompatPreferenceActivity() {
             }
             return super.onOptionsItemSelected(item)
         }
-    }
+    } // GeneralPreferenceFragment ends
 
     /**
      * This fragment shows notification preferences only. It is used when the
@@ -135,7 +133,7 @@ class SettingsActivity : AppCompatPreferenceActivity() {
             }
             return super.onOptionsItemSelected(item)
         }
-    }
+    } // NotificationPreferenceFragment ends
 
     /**
      * This fragment shows data and sync preferences only. It is used when the
@@ -163,7 +161,7 @@ class SettingsActivity : AppCompatPreferenceActivity() {
             }
             return super.onOptionsItemSelected(item)
         }
-    }
+    } // DataSyncPreferenceFragment ends
 
     companion object {
 
@@ -171,19 +169,24 @@ class SettingsActivity : AppCompatPreferenceActivity() {
          * A preference value change listener that updates the preference's summary
          * to reflect its new value.
          */
-        private val sBindPreferenceSummaryToValueListener = Preference.OnPreferenceChangeListener { preference, value ->
-            val stringValue = value.toString()
+        private val sBindPreferenceSummaryToValueListener: Preference.OnPreferenceChangeListener =
+
+                Preference.OnPreferenceChangeListener {
+
+            preference, newValue ->
+
+            val stringValue = newValue.toString()
 
             if (preference is ListPreference) {
                 // For list preferences, look up the correct display value in
                 // the preference's 'entries' list.
-                val listPreference = preference
-                val index = listPreference.findIndexOfValue(stringValue)
+                //val listPreference = preference
+                val index = preference.findIndexOfValue(stringValue)
 
                 // Set the summary to reflect the new value.
                 preference.setSummary(
                         if (index >= 0)
-                            listPreference.entries[index]
+                            preference.entries[index]
                         else
                             null)
 
@@ -214,15 +217,22 @@ class SettingsActivity : AppCompatPreferenceActivity() {
                 // simple string representation.
                 preference.summary = stringValue
             }
+                    /* True to update the state of the Preference with the new value */
             true
-        }
+        } // OnPreferenceChangeListener ends
 
         /**
          * Helper method to determine if the device has an extra-large screen. For
          * example, 10" tablets are extra-large.
          */
         private fun isXLargeTablet(context: Context): Boolean {
-            return context.resources.configuration.screenLayout and Configuration.SCREENLAYOUT_SIZE_MASK >= Configuration.SCREENLAYOUT_SIZE_XLARGE
+            /*
+            * value indicating the screen is at least approximately 720x960 dp units,
+            * corresponds to the xlarge resource qualifier
+            *
+            * */
+            return context.resources.configuration.screenLayout  and
+                    Configuration.SCREENLAYOUT_SIZE_MASK >= Configuration.SCREENLAYOUT_SIZE_XLARGE
         }
 
         /**
@@ -238,12 +248,20 @@ class SettingsActivity : AppCompatPreferenceActivity() {
             // Set the listener to watch for value changes.
             preference.onPreferenceChangeListener = sBindPreferenceSummaryToValueListener
 
-            // Trigger the listener immediately with the preference's
-            // current value.
-            sBindPreferenceSummaryToValueListener.onPreferenceChange(preference,
-                    PreferenceManager
-                            .getDefaultSharedPreferences(preference.context)
-                            .getString(preference.key, ""))
+            // Boolean preference
+            if (preference.key == PreferenceKeys.ANIMATION_KEY)
+                sBindPreferenceSummaryToValueListener.onPreferenceChange(preference,
+                        PreferenceManager
+                                .getDefaultSharedPreferences(preference.context)
+                                .getBoolean(preference.key, true))
+            else { // String preference
+
+                // Trigger the listener immediately with the preference's new value.
+                sBindPreferenceSummaryToValueListener.onPreferenceChange(preference,
+                        PreferenceManager
+                                .getDefaultSharedPreferences(preference.context)
+                                .getString(preference.key, ""))
+            }
         }
-    }
+    } // Companion ends
 }
---
+++ app/src/main/java/cat/cristina/pep/jbcnconffeedback/utils/PreferenceKeys.kt
@@ -0,0 +1,8 @@
+package cat.cristina.pep.jbcnconffeedback.utils
+
+class PreferenceKeys {
+
+    companion object {
+        const val ANIMATION_KEY = "animation_key"
+    }
+}
