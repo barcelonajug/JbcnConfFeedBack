8e7b04b7625aa3618c5e9efd479694e6c2910465
casensio <cristina.asensio.munoz@gmail.com>
2018-05-19 11:29:56 +0200

    disable lateral menu qhen on vote fragment, both click and swap

--- app/src/main/java/cat/cristina/pep/jbcnconffeedback/activity/MainActivity.kt
+++ app/src/main/java/cat/cristina/pep/jbcnconffeedback/activity/MainActivity.kt
@@ -101,7 +101,7 @@ class MainActivity :
 
         val toggle = ActionBarDrawerToggle(
                 this, drawer_layout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
-        drawer_layout.addDrawerListener(toggle)
+
         toggle.syncState()
 
         nav_view.setNavigationItemSelectedListener(this)
