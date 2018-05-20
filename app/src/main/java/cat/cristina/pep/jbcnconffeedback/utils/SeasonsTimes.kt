e72628b14c4126020fee0c2059ddaca0a5dbfc16
Pep Mendez <jmendez1@xtec.cat>
2018-05-20 10:47:50 +0200

    SeasonsTimes added two new methods and added TalksLocation enum class

--- app/src/main/java/cat/cristina/pep/jbcnconffeedback/utils/SeasonsTimes.kt
+++ app/src/main/java/cat/cristina/pep/jbcnconffeedback/utils/SeasonsTimes.kt
@@ -3,101 +3,41 @@ package cat.cristina.pep.jbcnconffeedback.utils
 import java.util.*
 
 
-/* LocalTime not available until API 26, current is 23-24 */
+/*
+* LocalTime not available until API 26, current is 23-24
+*
+* Use the method getTime() from Calendar to convert to a Date object
+*
+* */
+
+/* Offset in minutes  */
+private const val OFFSET = 15
+
 enum class SeasonsTimes {
 
     MON_SE1 {
-        override fun getStartTime() = GregorianCalendar(2018, 6, 11, 9, 40)
-        override fun getEndTime() = GregorianCalendar(2018, 6, 11, 10, 30)
-    },
-    MON_SE2 {
-        override fun getStartTime() = GregorianCalendar(2018, 6, 11, 9, 40)
-        override fun getEndTime() = GregorianCalendar(2018, 6, 11, 10, 30)
-    },
-    MON_SE3 {
-        override fun getStartTime() = GregorianCalendar(2018, 6, 11, 9, 40)
-        override fun getEndTime() = GregorianCalendar(2018, 6, 11, 10, 30)
-    },
-    MON_SE4 {
-        override fun getStartTime() = GregorianCalendar(2018, 6, 11, 9, 40)
-        override fun getEndTime() = GregorianCalendar(2018, 6, 11, 10, 30)
-    },
-    MON_SE5 {
-        override fun getStartTime() = GregorianCalendar(2018, 6, 11, 9, 40)
-        override fun getEndTime() = GregorianCalendar(2018, 6, 11, 10, 30)
-    },
-    MON_SE6 {
-        override fun getStartTime() = GregorianCalendar(2018, 6, 11, 9, 40)
-        override fun getEndTime() = GregorianCalendar(2018, 6, 11, 10, 30)
-    },
-    MON_SE7 {
-        override fun getStartTime() = GregorianCalendar(2018, 6, 11, 9, 40)
-        override fun getEndTime() = GregorianCalendar(2018, 6, 11, 10, 30)
-    },
-    TUE_SE1 {
-        override fun getStartTime() = GregorianCalendar(2018, 6, 11, 9, 40)
-        override fun getEndTime() = GregorianCalendar(2018, 6, 11, 10, 30)
-    },
-    TUE_SE2 {
-        override fun getStartTime() = GregorianCalendar(2018, 6, 11, 9, 40)
-        override fun getEndTime() = GregorianCalendar(2018, 6, 11, 10, 30)
-    },
-    TUE_SE3 {
-        override fun getStartTime() = GregorianCalendar(2018, 6, 11, 9, 40)
-        override fun getEndTime() = GregorianCalendar(2018, 6, 11, 10, 30)
-    },
-    TUE_SE4 {
-        override fun getStartTime() = GregorianCalendar(2018, 6, 11, 9, 40)
-        override fun getEndTime() = GregorianCalendar(2018, 6, 11, 10, 30)
-    },
-    TUE_SE5 {
-        override fun getStartTime() = GregorianCalendar(2018, 6, 11, 9, 40)
-        override fun getEndTime() = GregorianCalendar(2018, 6, 11, 10, 30)
-    },
-    TUE_SE6 {
-        override fun getStartTime() = GregorianCalendar(2018, 6, 11, 9, 40)
-        override fun getEndTime() = GregorianCalendar(2018, 6, 11, 10, 30)
-    },
-    TUE_SE7 {
-        override fun getStartTime() = GregorianCalendar(2018, 6, 11, 9, 40)
-        override fun getEndTime() = GregorianCalendar(2018, 6, 11, 10, 30)
-    },
-    WED_SE1 {
-        override fun getStartTime() = GregorianCalendar(2018, 6, 11, 9, 40)
-        override fun getEndTime() = GregorianCalendar(2018, 6, 11, 10, 30)
-    },
-    WED_SE2 {
-        override fun getStartTime() = GregorianCalendar(2018, 6, 11, 9, 40)
-        override fun getEndTime() = GregorianCalendar(2018, 6, 11, 10, 30)
-    },
-    WED_SE3 {
-        override fun getStartTime() = GregorianCalendar(2018, 6, 11, 9, 40)
-        override fun getEndTime() = GregorianCalendar(2018, 6, 11, 10, 30)
-    },
-    WED_SE4 {
-        override fun getStartTime() = GregorianCalendar(2018, 6, 11, 9, 40)
-        override fun getEndTime() = GregorianCalendar(2018, 6, 11, 10, 30)
-    },
-    WED_SE5 {
-        override fun getStartTime() = GregorianCalendar(2018, 6, 11, 9, 40)
-        override fun getEndTime() = GregorianCalendar(2018, 6, 11, 10, 30)
-    },
-    WED_SE6 {
-        override fun getStartTime() = GregorianCalendar(2018, 6, 11, 9, 40)
-        override fun getEndTime() = GregorianCalendar(2018, 6, 11, 10, 30)
-    },
-    WED_SE7 {
-        override fun getStartTime() = GregorianCalendar(2018, 6, 11, 9, 40)
-        override fun getEndTime() = GregorianCalendar(2018, 6, 11, 10, 30)
+        override fun getStartTime(): Calendar = GregorianCalendar(2018, 6, 11, 9, 40)
+        override fun getEndTime(): Calendar  = GregorianCalendar(2018, 6, 11, 10, 30)
+        override fun getStartTimeMinusOffset(): Date {
+            val calendar = getStartTime()
+            calendar.set(Calendar.MINUTE, calendar.get(Calendar.MINUTE - OFFSET))
+            return calendar.time
+        }
+        override fun getEndTimePlusOffset(): Date {
+            val calendar = getEndTime()
+            calendar.set(Calendar.MINUTE, calendar.get(Calendar.MINUTE + OFFSET))
+            return calendar.time
+        }
     };
 
     abstract fun getStartTime(): Calendar
     abstract fun getEndTime(): Calendar
+    abstract fun getStartTimeMinusOffset(): Date
+    abstract fun getEndTimePlusOffset(): Date
 
 }
 
 fun main(args: Array<String>) {
     val s: SeasonsTimes = SeasonsTimes.MON_SE1
-
 
 }
---
+++ app/src/main/java/cat/cristina/pep/jbcnconffeedback/utils/TalksLocations.kt
@@ -0,0 +1,15 @@
+package cat.cristina.pep.jbcnconffeedback.utils
+
+
+/*
+* Important: this room name MUST be the same name as the room name configured in settings
+* for each tablet.
+*
+* */
+
+enum class TalksLocations {
+
+    MON_TC1 { override fun getRoomName() = "Room #1" },;
+
+    abstract fun getRoomName(): String
+}
