d024186b6b79ce52fce192c1e252dc28f422d20a
Simone <simone.masoni@gmail.com>
2019-05-08 08:21:11 +0200

    Set Aba English Room name

--- app/src/main/java/cat/cristina/pep/jbcnconffeedback/utils/TalksLocations.kt
+++ app/src/main/java/cat/cristina/pep/jbcnconffeedback/utils/TalksLocations.kt
@@ -9,12 +9,12 @@ package cat.cristina.pep.jbcnconffeedback.utils
 
 enum class TalksLocations {
 
-    MON_TC1 { override fun getRoomName() = "Room 1" },
+    MON_TC1 { override fun getRoomName() = "Aba English Room" },
     MON_TC2 { override fun getRoomName() = "Room 2" },
     MON_TC3 { override fun getRoomName() = "Room 3" },
     MON_TC4 { override fun getRoomName() = "Room 4" },
     MON_TC5 { override fun getRoomName() = "Room 5" },
-    TUE_TC1 { override fun getRoomName() = "Room 1" },
+    TUE_TC1 { override fun getRoomName() = "Aba English Room" },
     TUE_TC2 { override fun getRoomName() = "Room 2" },
     TUE_TC3 { override fun getRoomName() = "Room 3" },
     TUE_TC4 { override fun getRoomName() = "Room 4" },
