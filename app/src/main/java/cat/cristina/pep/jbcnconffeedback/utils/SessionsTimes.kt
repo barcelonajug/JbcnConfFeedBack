f6a5558662669dec45e19e03c590f2fb1ffc84ec
jguitart <jguitart@biid.com>
2022-07-10 22:27:15 +0200

    Updates session times to 2022

--- app/src/main/java/cat/cristina/pep/jbcnconffeedback/utils/SessionsTimes.kt
+++ app/src/main/java/cat/cristina/pep/jbcnconffeedback/utils/SessionsTimes.kt
@@ -16,96 +16,95 @@ private const val OFFSET = 15
 enum class SessionsTimes {
 
     MON_SE1 {
-        override fun getStartTalkDateTime(): Calendar = GregorianCalendar(2019, Calendar.MAY, 27, 9, 20)
-        override fun getEndTalkDateTime(): Calendar  = GregorianCalendar(2019, Calendar.MAY, 27, 9, 55)
+        override fun getStartTalkDateTime(): Calendar = GregorianCalendar(2022, Calendar.JULY, 18, 9, 15)
+        override fun getEndTalkDateTime(): Calendar  = GregorianCalendar(2022, Calendar.JULY, 18, 9, 55)
     },
 
     MON_SE2 {
-        override fun getStartTalkDateTime(): Calendar = GregorianCalendar(2019, Calendar.MAY, 27, 10, 0)
-        override fun getEndTalkDateTime(): Calendar  = GregorianCalendar(2019, Calendar.MAY, 27, 10, 35)
+        override fun getStartTalkDateTime(): Calendar = GregorianCalendar(2022, Calendar.JULY, 18, 10, 0)
+        override fun getEndTalkDateTime(): Calendar  = GregorianCalendar(2022, Calendar.JULY, 18, 10, 35)
     },
 
     MON_SE3 {
-        override fun getStartTalkDateTime(): Calendar = GregorianCalendar(2019, Calendar.MAY, 27, 11, 10)
-        override fun getEndTalkDateTime(): Calendar  = GregorianCalendar(2019, Calendar.MAY, 27, 12, 0)
+        override fun getStartTalkDateTime(): Calendar = GregorianCalendar(2022, Calendar.JULY, 18, 11, 10)
+        override fun getEndTalkDateTime(): Calendar  = GregorianCalendar(2022, Calendar.JULY, 18, 12, 0)
     },
 
     MON_SE4 {
-        override fun getStartTalkDateTime(): Calendar = GregorianCalendar(2019, Calendar.MAY, 27, 12, 15)
-        override fun getEndTalkDateTime(): Calendar  = GregorianCalendar(2019, Calendar.MAY, 27, 13, 5)
+        override fun getStartTalkDateTime(): Calendar = GregorianCalendar(2022, Calendar.JULY, 18, 12, 15)
+        override fun getEndTalkDateTime(): Calendar  = GregorianCalendar(2022, Calendar.JULY, 18, 13, 5)
     },
 
     MON_SE5 {
-        override fun getStartTalkDateTime(): Calendar = GregorianCalendar(2019, Calendar.MAY, 27, 14, 30)
-        override fun getEndTalkDateTime(): Calendar  = GregorianCalendar(2019, Calendar.MAY, 27, 15, 20)
+        override fun getStartTalkDateTime(): Calendar = GregorianCalendar(2022, Calendar.JULY, 18, 14, 30)
+        override fun getEndTalkDateTime(): Calendar  = GregorianCalendar(2022, Calendar.JULY, 18, 15, 20)
     },
 
     MON_SE6 {
-        override fun getStartTalkDateTime(): Calendar = GregorianCalendar(2019, Calendar.MAY, 27, 15, 35)
-        override fun getEndTalkDateTime(): Calendar  = GregorianCalendar(2019, Calendar.MAY, 27, 16, 25)
+        override fun getStartTalkDateTime(): Calendar = GregorianCalendar(2022, Calendar.JULY, 18, 15, 35)
+        override fun getEndTalkDateTime(): Calendar  = GregorianCalendar(2022, Calendar.JULY, 18, 16, 25)
     },
 
     MON_SE7 {
-        override fun getStartTalkDateTime(): Calendar = GregorianCalendar(2019, Calendar.MAY, 27, 17, 5)
-        override fun getEndTalkDateTime(): Calendar  = GregorianCalendar(2019, Calendar.MAY, 27, 17, 55)
+        override fun getStartTalkDateTime(): Calendar = GregorianCalendar(2022, Calendar.JULY, 18, 17, 5)
+        override fun getEndTalkDateTime(): Calendar  = GregorianCalendar(2022, Calendar.JULY, 18, 17, 55)
     },
-
     MON_SE8 {
-        override fun getStartTalkDateTime(): Calendar = GregorianCalendar(2019, Calendar.MAY, 27, 18, 10)
-        override fun getEndTalkDateTime(): Calendar  = GregorianCalendar(2019, Calendar.MAY, 27, 19, 0)
+        override fun getStartTalkDateTime(): Calendar = GregorianCalendar(2022, Calendar.JULY, 18, 18, 10)
+        override fun getEndTalkDateTime(): Calendar  = GregorianCalendar(2022, Calendar.JULY, 18, 19, 0)
     },
 
     TUE_SE1 {
-        override fun getStartTalkDateTime(): Calendar = GregorianCalendar(2019, Calendar.MAY, 28, 9, 0)
-        override fun getEndTalkDateTime(): Calendar  = GregorianCalendar(2019, Calendar.MAY, 28, 9, 40)
+        override fun getStartTalkDateTime(): Calendar = GregorianCalendar(2022, Calendar.JULY, 19, 9, 0)
+        override fun getEndTalkDateTime(): Calendar  = GregorianCalendar(2022, Calendar.JULY, 19, 9, 50)
     },
 
     TUE_SE2 {
-        override fun getStartTalkDateTime(): Calendar = GregorianCalendar(2019, Calendar.MAY, 28, 9, 45)
-        override fun getEndTalkDateTime(): Calendar  = GregorianCalendar(2019, Calendar.MAY, 28, 10, 35)
+        override fun getStartTalkDateTime(): Calendar = GregorianCalendar(2022, Calendar.JULY, 19, 10, 0)
+        override fun getEndTalkDateTime(): Calendar  = GregorianCalendar(2022, Calendar.JULY, 19, 10, 50)
     },
 
     TUE_SE3 {
-        override fun getStartTalkDateTime(): Calendar = GregorianCalendar(2019, Calendar.MAY, 28, 11, 10)
-        override fun getEndTalkDateTime(): Calendar  = GregorianCalendar(2019, Calendar.MAY, 28, 12, 0)
+        override fun getStartTalkDateTime(): Calendar = GregorianCalendar(2022, Calendar.JULY, 19, 11, 10)
+        override fun getEndTalkDateTime(): Calendar  = GregorianCalendar(2022, Calendar.JULY, 19, 12, 0)
     },
 
     TUE_SE4 {
-        override fun getStartTalkDateTime(): Calendar = GregorianCalendar(2019, Calendar.MAY, 28, 12, 15)
-        override fun getEndTalkDateTime(): Calendar  = GregorianCalendar(2019, Calendar.MAY, 28, 13, 5)
+        override fun getStartTalkDateTime(): Calendar = GregorianCalendar(2022, Calendar.JULY, 19, 12, 15)
+        override fun getEndTalkDateTime(): Calendar  = GregorianCalendar(2022, Calendar.JULY, 19, 13, 5)
     },
 
     TUE_SE5 {
-        override fun getStartTalkDateTime(): Calendar = GregorianCalendar(2019, Calendar.MAY, 28, 14, 30)
-        override fun getEndTalkDateTime(): Calendar  = GregorianCalendar(2019, Calendar.MAY, 28, 15, 20)
+        override fun getStartTalkDateTime(): Calendar = GregorianCalendar(2022, Calendar.JULY, 19, 14, 30)
+        override fun getEndTalkDateTime(): Calendar  = GregorianCalendar(2022, Calendar.JULY, 19, 15, 20)
     },
 
     TUE_SE6 {
-        override fun getStartTalkDateTime(): Calendar = GregorianCalendar(2019, Calendar.MAY, 28, 15, 25)
-        override fun getEndTalkDateTime(): Calendar  = GregorianCalendar(2019, Calendar.MAY, 28, 16, 15)
+        override fun getStartTalkDateTime(): Calendar = GregorianCalendar(2022, Calendar.JULY, 19, 15, 25)
+        override fun getEndTalkDateTime(): Calendar  = GregorianCalendar(2022, Calendar.JULY, 19, 16, 15)
     },
 
     TUE_SE7 {
-        override fun getStartTalkDateTime(): Calendar = GregorianCalendar(2019, Calendar.MAY, 28, 16, 55)
-        override fun getEndTalkDateTime(): Calendar  = GregorianCalendar(2019, Calendar.MAY, 28, 17, 45)
+        override fun getStartTalkDateTime(): Calendar = GregorianCalendar(2022, Calendar.JULY, 19, 16, 55)
+        override fun getEndTalkDateTime(): Calendar  = GregorianCalendar(2022, Calendar.JULY, 19, 17, 45)
     },
     TUE_SE8 {
-        override fun getStartTalkDateTime(): Calendar = GregorianCalendar(2019, Calendar.MAY, 28, 18, 0)
-        override fun getEndTalkDateTime(): Calendar  = GregorianCalendar(2019, Calendar.MAY, 28, 18, 40)
+        override fun getStartTalkDateTime(): Calendar = GregorianCalendar(2022, Calendar.JULY, 19, 18, 0)
+        override fun getEndTalkDateTime(): Calendar  = GregorianCalendar(2022, Calendar.JULY, 19, 18, 40)
     },
     TUE_SE9 {
-        override fun getStartTalkDateTime(): Calendar = GregorianCalendar(  2019, Calendar.MAY, 28, 18, 0)
-        override fun getEndTalkDateTime(): Calendar  = GregorianCalendar(2019, Calendar.MAY, 28, 18, 40)
+        override fun getStartTalkDateTime(): Calendar = GregorianCalendar(  2022, Calendar.JULY, 19, 18, 45)
+        override fun getEndTalkDateTime(): Calendar  = GregorianCalendar(2022, Calendar.JULY, 19, 19, 0)
     },
 
     WED_SE1 {
-        override fun getStartTalkDateTime(): Calendar = GregorianCalendar(2019, Calendar.MAY, 29, 9, 0)
-        override fun getEndTalkDateTime(): Calendar  = GregorianCalendar(2019, Calendar.MAY, 29, 10, 55)
+        override fun getStartTalkDateTime(): Calendar = GregorianCalendar(2022, Calendar.JULY, 20, 9, 0)
+        override fun getEndTalkDateTime(): Calendar  = GregorianCalendar(2022, Calendar.JULY, 20, 10, 55)
     },
 
     WED_SE2 {
-        override fun getStartTalkDateTime(): Calendar = GregorianCalendar(2019, Calendar.MAY, 29, 11, 40)
-        override fun getEndTalkDateTime(): Calendar  = GregorianCalendar(2019, Calendar.MAY, 29, 13, 35)
+        override fun getStartTalkDateTime(): Calendar = GregorianCalendar(2022, Calendar.JULY, 20, 11, 40)
+        override fun getEndTalkDateTime(): Calendar  = GregorianCalendar(2022, Calendar.JULY, 20, 13, 35)
     };
 
     abstract fun getStartTalkDateTime(): Calendar
